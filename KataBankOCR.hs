1b1d6e8fabdcb4d32297425662ef8617ec8e5d98
Jon Schoning <jonschoning@gmail.com>
2014-03-12 01:10:08 -0500

    impl use cases 1-3

--- KataBankOCR.hs
+++ KataBankOCR.hs
@@ -1,10 +1,48 @@
-module KataBankOCR (Account, createAccount, parseAccount) where
+module KataBankOCR (Status (..), Account, createAccount, parseAccount, isChecksumValid, withStatus) where
 
-data Account = Account
-               deriving (Eq, Show)
+import Control.Applicative (liftA2)
+import Data.Char (digitToInt, isDigit)
+import Data.List (transpose)
+import Data.List.Split (chunksOf)
+import qualified Data.Map.Strict as M
 
-parseAccount :: String -> Account
-parseAccount = undefined
+type Account = String
+
+type DigitLines = [String]
+
+data Status = OK | ERR | ILL
+              deriving (Eq, Show, Enum)
 
 createAccount :: String -> Account
-createAccount = undefined
+createAccount = id
+
+parseAccount :: String -> Account
+parseAccount = concatMap (maybe "?" show . parseAccountDigit) . makeDigitLines
+
+parseAccountDigit :: DigitLines -> Maybe Integer
+parseAccountDigit  = (flip M.lookup ocrMap) . concat
+
+makeDigitLines :: String -> [DigitLines]
+makeDigitLines = transpose . map (chunksOf 3) . lines
+  
+isChecksumValid :: Account -> Bool
+isChecksumValid a 
+  | any (not . isDigit) a = False
+  | otherwise = (0 ==) . (`mod` 11) . sum . zipWith (*) [9, 8..1] . map digitToInt $ a
+
+withStatus :: Account -> String
+withStatus = liftA2 (++) id (disp . status)
+  where disp OK = ""
+        disp s = ' ' : show s
+
+status :: Account -> Status
+status s 
+  | any (== '?') s = ILL
+  | isChecksumValid s = OK
+  | otherwise = ERR
+
+ocrMap :: M.Map String Integer
+ocrMap = M.fromList $ (`zip` [0..9]) . map concat . makeDigitLines $
+         " _     _  _     _  _  _  _  _ \n\ 
+         \| |  | _| _||_||_ |_   ||_||_|\n\ 
+         \|_|  ||_  _|  | _||_|  ||_| _|\n"   
--- kataBankOCR_test.hs
+++ kataBankOCR_test.hs
@@ -1,7 +1,7 @@
 import Test.HUnit (Assertion, (@=?), runTestTT, Test(..), Counts(..))
 import System.Exit (ExitCode(..), exitWith)
 
-import KataBankOCR (Account, parseAccount, createAccount)
+import KataBankOCR (Status (..), Account, parseAccount, createAccount, isChecksumValid, withStatus)
 
 exitProperly :: IO Counts -> IO ()
 exitProperly m = do
@@ -19,59 +19,74 @@ acct :: String -> Account
 acct = createAccount
 
 kataBankOCRTests :: [Test]
-kataBankOCRTests =
-  [testCase "use case 1" $ do   
-   acct "000000000" @=? parseAccount "\
-\ _  _  _  _  _  _  _  _  _ \n\
-\| || || || || || || || || |\n\
-\|_||_||_||_||_||_||_||_||_|\n"
-   acct "111111111" @=? parseAccount "\
-\                           \n\
-\  |  |  |  |  |  |  |  |  |\n\
-\  |  |  |  |  |  |  |  |  |\n"
+kataBankOCRTests = [
 
-   acct "222222222" @=? parseAccount "\
-\ _  _  _  _  _  _  _  _  _ \n\ 
-\ _| _| _| _| _| _| _| _| _|\n\ 
-\|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"   
+  testCase "use case 1: parse account numbers" $ do   
 
-   acct "333333333" @=? parseAccount "\
-\ _  _  _  _  _  _  _  _  _ \n\ 
-\ _| _| _| _| _| _| _| _| _|\n\ 
-\ _| _| _| _| _| _| _| _| _|\n"   
+     acct "000000000" @=? parseAccount "\
+          \ _  _  _  _  _  _  _  _  _ \n\
+          \| || || || || || || || || |\n\
+          \|_||_||_||_||_||_||_||_||_|\n"
 
-   acct "444444444" @=? parseAccount "\
-\                           \n\ 
-\|_||_||_||_||_||_||_||_||_|\n\ 
-\  |  |  |  |  |  |  |  |  |\n"   
+     acct "111111111" @=? parseAccount "\
+          \                           \n\
+          \  |  |  |  |  |  |  |  |  |\n\
+          \  |  |  |  |  |  |  |  |  |\n"
 
-   acct "555555555" @=? parseAccount "\
-\ _  _  _  _  _  _  _  _  _ \n\ 
-\|_ |_ |_ |_ |_ |_ |_ |_ |_ \n\ 
-\ _| _| _| _| _| _| _| _| _|\n"   
+     acct "222222222" @=? parseAccount "\
+          \ _  _  _  _  _  _  _  _  _ \n\ 
+          \ _| _| _| _| _| _| _| _| _|\n\ 
+          \|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"   
 
-   acct "666666666" @=? parseAccount "\
-\ _  _  _  _  _  _  _  _  _ \n\ 
-\|_ |_ |_ |_ |_ |_ |_ |_ |_ \n\ 
-\|_||_||_||_||_||_||_||_||_|\n"   
+     acct "333333333" @=? parseAccount "\
+          \ _  _  _  _  _  _  _  _  _ \n\ 
+          \ _| _| _| _| _| _| _| _| _|\n\ 
+          \ _| _| _| _| _| _| _| _| _|\n"   
 
-   acct "777777777" @=? parseAccount "\
-\ _  _  _  _  _  _  _  _  _ \n\ 
-\  |  |  |  |  |  |  |  |  |\n\ 
-\  |  |  |  |  |  |  |  |  |\n"   
+     acct "444444444" @=? parseAccount "\
+          \                           \n\ 
+          \|_||_||_||_||_||_||_||_||_|\n\ 
+          \  |  |  |  |  |  |  |  |  |\n"   
 
-   acct "888888888" @=? parseAccount "\
-\ _  _  _  _  _  _  _  _  _ \n\ 
-\|_||_||_||_||_||_||_||_||_|\n\ 
-\|_||_||_||_||_||_||_||_||_|\n"   
+     acct "555555555" @=? parseAccount "\
+          \ _  _  _  _  _  _  _  _  _ \n\ 
+          \|_ |_ |_ |_ |_ |_ |_ |_ |_ \n\ 
+          \ _| _| _| _| _| _| _| _| _|\n"   
 
-   acct "999999999" @=? parseAccount "\
-\ _  _  _  _  _  _  _  _  _ \n\ 
-\|_||_||_||_||_||_||_||_||_|\n\ 
-\ _| _| _| _| _| _| _| _| _|\n"   
+     acct "666666666" @=? parseAccount "\
+          \ _  _  _  _  _  _  _  _  _ \n\ 
+          \|_ |_ |_ |_ |_ |_ |_ |_ |_ \n\ 
+          \|_||_||_||_||_||_||_||_||_|\n"   
 
-   acct "123456789" @=? parseAccount "\
-\    _  _     _  _  _  _  _ \n\ 
-\  | _| _||_||_ |_   ||_||_|\n\ 
-\  ||_  _|  | _||_|  ||_| _|\n"   
+     acct "777777777" @=? parseAccount "\
+          \ _  _  _  _  _  _  _  _  _ \n\ 
+          \  |  |  |  |  |  |  |  |  |\n\ 
+          \  |  |  |  |  |  |  |  |  |\n"   
+
+     acct "888888888" @=? parseAccount "\
+          \ _  _  _  _  _  _  _  _  _ \n\ 
+          \|_||_||_||_||_||_||_||_||_|\n\ 
+          \|_||_||_||_||_||_||_||_||_|\n"   
+
+     acct "999999999" @=? parseAccount "\
+          \ _  _  _  _  _  _  _  _  _ \n\ 
+          \|_||_||_||_||_||_||_||_||_|\n\ 
+          \ _| _| _| _| _| _| _| _| _|\n"   
+
+     acct "123456789" @=? parseAccount "\
+          \    _  _     _  _  _  _  _ \n\ 
+          \  | _| _||_||_ |_   ||_||_|\n\ 
+          \  ||_  _|  | _||_|  ||_| _|\n"
+
+   , testCase "use case 2: calculate checksums" $ do    
+
+     True  @=? isChecksumValid (acct "345882865")
+     False @=? isChecksumValid (acct "3458?2865")
+     False @=? isChecksumValid (acct "345882866") 
+
+   , testCase "use case 3: show account status" $ do    
+
+     "457508000"     @=? withStatus (acct "457508000")
+     "664371495 ERR" @=? withStatus (acct "664371495")
+     "86110??36 ILL" @=? withStatus (acct "86110??36") 
   ]
