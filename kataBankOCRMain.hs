dba203f1e14c4e1f19a65ec5ad7cb6e7bdad7cde
Jon Schoning <jonschoning@gmail.com>
2014-03-13 15:08:12 -0500

    only guess if invalid

--- kataBankOCRMain.hs
+++ kataBankOCRMain.hs
@@ -11,7 +11,8 @@ main = do args <- getArgs
             bracket (openFile (head args) ReadMode) hClose loop
   where 
     read4lines = sequence . replicate 4 . hGetLine
-    process4Lines = (putStrLn . show . guess . parseAccount =<<)
+    process4Lines = (putStrLn . show . guessIfInvalid . parseAccount =<<)
+    guessIfInvalid acct = if isValid (fst acct) then fst acct else guess acct
     loop handle = do
       isEof <- hIsEOF handle
       if isEof then return () else (process4Lines (read4lines handle) >> loop handle) 
